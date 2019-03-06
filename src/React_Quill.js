bca27c6905d33c54f97a6e87d25de7af8e85480d
Jeff Pinckney <jeffpinckney.90@gmail.com>
2019-03-06 15:34:41 -0700

    initial commit

--- src/React_Quill.js
+++ src/React_Quill.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import './App.css';
-import ReactQuill from 'react-quill'
+import React, { Component } from 'react'
 import 'react-quill/dist/quill.snow.css'
-// import sticky_toolbar from '../src/module-sticky-toolbar'
 
 
-
-class Editor extends React.Component {
+class React_Quill extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorHtml: '',
       text: ''
     }
     this.handleChange = this.handleChange.bind(this)
   }
-  
-  // handleHtmlChange() {
-  //   this.setState({
-  //     editorHtml: html
-  //   })
-  // }
 
   handleChange(value) {
     this.setState({
@@ -32,21 +21,18 @@ class Editor extends React.Component {
     toolbar: [
       // [{'sticky_toolbar': true}]
       [ { 'header': [ 1, 2, 3, 4, 5, 6, false ] } ],
-      [ { 'font': [] } ],
       [ 'bold', 'italic', 'underline' ],
       [ { 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' } ],
       [ { 'color': [] }, { 'background': [] } ],
-      [ 'link' ],
-      [ 'code-block' ],
-
+      [ 'link' ], [ 'emoji' ], [ 'code-block' ]
     ],
   }
 
   formats = [
-    'header', 'font',
+    'header',
     'bold', 'italic', 'underline',
     'list', 'bullet', 'indent',
-    'color', 'background', 'size', 'link', 'code-block'
+    'color', 'size', 'link', 'code-block'
   ]
 
 
@@ -57,19 +43,16 @@ class Editor extends React.Component {
         <p>Hello World</p>
         <p>some initial <strong>bold</strong> text</p>
         <p><br></br></p>
-        <div className='quill'>
-          <ReactQuill
-            theme="snow"
-            value={ this.state.text }
-            onChange={ this.handleChange }
-            modules={ this.modules }
-            formats={ this.formats }
-            bounds={ '.app' }
-          />
-        </div>
+        <ReactQuill
+          theme="snow"
+          value={ this.state.text }
+          onChange={ this.handleChange }
+          modules={ this.modules }
+          formats={ this.formats }
+        />
       </div>
     );
   }
 }
 
-export default Editor;
+export default React_Quill;
